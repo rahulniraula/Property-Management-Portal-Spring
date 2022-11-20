681f0cbcdfbcb92829ce8f59cf2d03bdafe20a3d
Rahul Niraula <rahul.niraula@gmail.com>
2022-11-20 13:43:33 -0600

    initial

---
+++ src/main/java/com/mwa/property_management_portal/PropertyManagementPortalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mwa.property_management_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementPortalSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyManagementPortalSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mwa/property_management_portal/PropertyManagementPortalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwa.property_management_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementPortalSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
