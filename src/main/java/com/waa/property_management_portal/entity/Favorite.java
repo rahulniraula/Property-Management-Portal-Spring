df84f147829c4556bc888f0c61de84dc120709b7
sushanta-ach <sushantaacharjeebd@gmail.com>
2022-11-22 17:19:45 -0600

    added Favourite

--- src/main/java/com/waa/property_management_portal/entity/Favorite.java
+++ src/main/java/com/waa/property_management_portal/entity/Favorite.java
@@ -5,15 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FavListProperty {
+public class Favorite {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
+    private String name;
+
+    @ManyToMany
+    List<Property> favProperties;
 
 }
