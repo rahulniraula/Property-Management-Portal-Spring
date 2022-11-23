74b26d91783a8bfbe9656002c798d5c29171e909
Fuad <mohammad.fuad@miu.edu>
2022-11-23 14:41:53 -0600

    Validation messages for PropertyDto

--- src/main/java/com/waa/property_management_portal/entity/dto/request/PropertyDtoReq.java
+++ src/main/java/com/waa/property_management_portal/entity/dto/request/PropertyDtoReq.java
@@ -4,11 +4,20 @@ import com.waa.property_management_portal.entity.Address;
 import com.waa.property_management_portal.entity.PropertyDetails;
 import com.waa.property_management_portal.enums.PropertyType;
 
+import javax.validation.constraints.NotEmpty;
+
 public class PropertyDtoReq {
     private long id;
+
+    @NotEmpty(message = "Please provide a price for the property.")
     private double price;
+
+    @NotEmpty(message = "Please provide a title for the property.")
     private String title;
+
+    @NotEmpty(message = "Please provide area in sqft unit.")
     private double area;
+
     private PropertyType propertyType;
     private Address address;
 
