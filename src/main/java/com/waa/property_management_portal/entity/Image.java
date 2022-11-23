0e4ed8d3c3c7fe2794f45c9189cfe8ac6b85033c
Fuad <mohammad.fuad@miu.edu>
2022-11-22 21:18:36 -0600

    Image and Property relation created

--- src/main/java/com/waa/property_management_portal/entity/Image.java
+++ src/main/java/com/waa/property_management_portal/entity/Image.java
@@ -4,19 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Images {
+public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String url;
     private Byte status;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
 }
