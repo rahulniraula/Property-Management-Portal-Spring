529ab6ae37ebd249ea06f1e0e8504f69bccb7b1e
Fuad <mohammad.fuad@miu.edu>
2022-11-22 18:36:34 -0600

    PropertyController fixed

--- src/main/java/com/waa/property_management_portal/controller/PropertyController.java
+++ src/main/java/com/waa/property_management_portal/controller/PropertyController.java
@@ -1,6 +1,8 @@
 package com.waa.property_management_portal.controller;
 
 import com.waa.property_management_portal.entity.Property;
+import com.waa.property_management_portal.entity.dto.request.PropertyDto;
+import com.waa.property_management_portal.service.PropertyService;
 import com.waa.property_management_portal.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +13,14 @@ import java.util.List;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PropertyController {
 
-    private UserService userService;
+    private PropertyService propertyService;
 
-    public PropertyController(UserService userService) {
-        this.userService = userService;
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
     }
 
     @GetMapping("/")
-    public List<Property> getProperties(@PathVariable long userId) {
-        return userService.findProperties(userId);
+    public List<PropertyDto> getProperties() {
+        return propertyService.findAll();
     }
 }
