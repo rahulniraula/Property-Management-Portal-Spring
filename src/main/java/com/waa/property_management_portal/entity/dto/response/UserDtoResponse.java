db0cede9380e6399294c31ae6795a4c48cd6c016
Rahul Niraula <rahul.niraula@gmail.com>
2022-11-22 20:55:05 -0600

    minor

--- src/main/java/com/waa/property_management_portal/entity/dto/response/UserDtoResponse.java
+++ src/main/java/com/waa/property_management_portal/entity/dto/response/UserDtoResponse.java
@@ -1,11 +1,13 @@
 package com.waa.property_management_portal.entity.dto.response;
 
+import com.waa.property_management_portal.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,4 +23,5 @@ public class UserDtoResponse {
     private String lastname;
     @NotEmpty
     private String phoneNumber;
+    private List<Role> roles;
 }
