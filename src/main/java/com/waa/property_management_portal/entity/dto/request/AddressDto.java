9688f38dff2e15ed9797960033809dd604e59430
Fuad <mohammad.fuad@miu.edu>
2022-11-23 14:50:32 -0600

    Address validation messages

--- src/main/java/com/waa/property_management_portal/entity/dto/request/AddressDto.java
+++ src/main/java/com/waa/property_management_portal/entity/dto/request/AddressDto.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressDto {
     private long id;
+    @NotEmpty(message = "Please provide street.")
     private String street;
+    @NotEmpty(message = "Please provide a city.")
     private String city;
+    @NotEmpty(message = "Please provide a state.")
     private String state;
+    @NotEmpty(message = "Please provide a zip code.")
     private String zipCode;
+    @NotEmpty(message = "Please provide a country")
     private String country;
 }
