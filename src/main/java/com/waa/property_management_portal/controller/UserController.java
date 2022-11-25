c0ba20072476f2c32e0f7a0332892a54933ae26f
Fuad <mohammad.fuad@miu.edu>
2022-11-25 02:22:18 -0600

    PreAuthorize annotation works on method

--- src/main/java/com/waa/property_management_portal/controller/UserController.java
+++ src/main/java/com/waa/property_management_portal/controller/UserController.java
@@ -9,6 +9,7 @@ import com.waa.property_management_portal.enums.UserStatus;
 import com.waa.property_management_portal.service.UserService;
 import com.waa.property_management_portal.service.impl.AwesomeUserDetails;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class UserController {
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<UserDtoResponse> getAllUsers(){
         return userService.findAll();
     }
