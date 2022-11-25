970ff76127f4779dfea90945dd2e64786a7ba148
Rahul Niraula <rahul.niraula@gmail.com>
2022-11-24 22:52:21 -0600

    favorite dto added

--- src/main/java/com/waa/property_management_portal/entity/dto/request/FavoriteDto.java
+++ src/main/java/com/waa/property_management_portal/entity/dto/request/FavoriteDto.java
@@ -1,8 +1,11 @@
 package com.waa.property_management_portal.entity.dto.request;
 
+import com.waa.property_management_portal.entity.dto.response.PropertyDtoRes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class FavoriteDto {
     private Long id;
     private String name;
+    List<PropertyDtoRes> properties;
 }
